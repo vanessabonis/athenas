8ceb0b379af111fc276c05279d35b46847940500
vanessabonis <vanessa1302@gmail.com>
2024-05-28 00:12:53 -0300

    back

--- athenas-front/src/app/dto/filtro/PessoaFiltro.ts
+++
@@ -1,8 +0,0 @@
-export class PessoaFiltro {
-    constructor(
-        public nome: string | null = null,
-        public cpf: string | null = null,
-        public sexo: string | null = null
-      ) {}
-}
-  
